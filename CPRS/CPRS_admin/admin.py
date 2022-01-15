81df5abe6a39e9af4ce29d1e671fd4eba8b951e6
Huvinesh-Rajendran <huvitom99@outlook.com>
2022-01-15 22:57:56 +0800

    admin.py modified

--- CPRS/CPRS_admin/admin.py
+++ CPRS/CPRS_admin/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Students
+from .models import Students, Client, Supervisor, User
 # Register your models here.
 
+admin.site.register(User)
 admin.site.register(Students)
+admin.site.register(Client)
+admin.site.register(Supervisor)
+
