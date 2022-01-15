d4cc97daada700d85b64551660ef26d695fbbb3d
Huvinesh-Rajendran <huvitom99@outlook.com>
2022-01-15 22:57:23 +0800

    models.py modified

--- CPRS/CPRS_admin/models.py
+++ CPRS/CPRS_admin/models.py
@@ -1,6 +1,34 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser 
 # Create your models here.
 
+
+class User(AbstractUser):
+    is_student = models.BooleanField(default=False)
+    is_supervisor = models.BooleanField(default=False)
+    is_client = models.BooleanField(default=False)
+    
+
 class Students(models.Model):
-    pass 
+    user = models.OneToOneField(User,null=True,on_delete=models.CASCADE)
+    id = models.IntegerField(max_length=11,primary_key=True)
+    course_taken = models.CharField(max_length=50,null=True)
+    specialization = models.CharField(max_length=50,null=True)
+    area_of_interest = models.CharField(max_length=50,null=True) 
+
+    
+
+
+class Client(models.Model):
+    user = models.OneToOneField(User,null=True,on_delete=models.CASCADE)
+    id = models.IntegerField(max_length=20,primary_key=True)
+    company = models.CharField(max_length=50,null=True)
+
+
+
+class Supervisor(models.Model):
+    user = models.OneToOneField(User,null=True,on_delete=models.CASCADE)
+    id = models.IntegerField(max_length=20,primary_key=True)
+
+
+
