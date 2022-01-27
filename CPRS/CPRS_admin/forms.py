aa25ee0c2b789bb228012f2ea2155dd7fbf04c8d
Huvinesh-Rajendran <huvitom99@outlook.com>
2022-01-27 14:29:42 +0800

    modified

--- CPRS/CPRS_admin/forms.py
+++ CPRS/CPRS_admin/forms.py
@@ -8,8 +8,9 @@ from django.contrib.admin.widgets import FilteredSelectMultiple
 
 
 class StudentSignUpForm(UserCreationForm):
-    pass
-
+    first_name = forms.CharField(max_length=30, required=True, label='First Name', help_text='Optional.')
+    last_name = forms.CharField(max_length=30, required=True, label='Last Name', help_text='Optional.')
+    email = forms.EmailField(max_length=254, label='Email', help_text='Required. Inform a valid email address.')
     class Meta(UserCreationForm.Meta):
         model = User
 
@@ -17,6 +18,9 @@ class StudentSignUpForm(UserCreationForm):
     def save(self):
         user = super().save(commit=False)
         user.is_student = True
+        user.first_name.add(*self.cleaned_data.get("first_name"))
+        user.last_name.add(*self.cleaned_data.get("last_name"))
+        user.email.add(*self.cleaned_data.get("email"))
         user.save()
         student = Student.objects.create(user=user)
 
@@ -54,6 +58,9 @@ class ClientSignUpForm(UserCreationForm):
 
 
 class SupervisorSignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=True, label='First Name', help_text='Optional.')
+    last_name = forms.CharField(max_length=30, required=True, label='Last Name', help_text='Optional.')
+    email = forms.EmailField(max_length=254, label='Email', help_text='Required. Inform a valid email address.')
     class Meta(UserCreationForm.Meta):
         model = User
 
