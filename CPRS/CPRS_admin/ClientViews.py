0b58cf666f328d6d6a9b3ae90402858b112021a3
Huvinesh-Rajendran <huvitom99@outlook.com>
2022-02-02 14:46:14 +0800

    add client views

---
+++ CPRS/CPRS_admin/ClientViews.py
@@ -0,0 +1,38 @@
+import json
+import requests
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.core.files.storage import FileSystemStorage
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.shortcuts import render
+from django.urls import reverse
+from django.views.decorators.csrf import csrf_exempt
+from .models import Client,Client_Request, StudentGroup
+from .forms import ClientRequestForm
+
+def client_request_group(request,group_id):
+    template_name = "client/client_request_group.html"
+    group = StudentGroup.objects.get(id=group_id)
+    if request.method == 'GET':
+        requestform = ClientRequestForm(request.GET or None)
+    elif request.method == 'POST':
+        requestform = StudentGroupModelForm(request.POST)
+        if requestform.is_valid():
+            requestform.client = request.user.client
+            requestform.group = group 
+            requestform.save()
+        return redirect("client_view_requests")
+    context = {'group':group,'form':requestform} 
+    return render(request,"client/client_request_group.html",context)
+
+def client_view_requests(request):
+    groups = StudentGroup.objects.get(client=request.user.client)
+    context = {'groups':groups,'form':form} 
+    return render(request,"client/client_view_request.html",context)
+
+def client_view_group_details(request,group_id):
+    group = StudentGroup.objects.get(id=group_id)
+    context = {'group':group,'form':form} 
+    return render(request,"client/client_view_group_details.html",context)
+
+
