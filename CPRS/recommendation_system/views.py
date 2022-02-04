1ef2a940147977304996f2349b1ced58ac40f340
Huvinesh-Rajendran <huvitom99@outlook.com>
2022-02-04 09:59:45 +0800

    modified

--- CPRS/recommendation_system/views.py
+++ CPRS/recommendation_system/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 import psycopg2
 import pandas as pd
-
+from .contentbased import * 
 
 def make_recommendations_view(request,group_id):
     connection = psycopg2.connect(
@@ -13,10 +13,16 @@ def make_recommendations_view(request,group_id):
             port="5432",
             database="test3",
         )
-    postgreSQL_group_Query = "select * from " + '"CPRS_admin_studentgroup"' + f" where id ={group_id}"
+    postgreSQL_student_Query = "select * from " + '"CPRS_admin_student"' + f" where group_id ={group_id}"
 
     postgreSQL_project_Query = "select * from " + '"CPRS_admin_project"' + f" where is_assigned = False"
-    df_group = pd.read_sql_query(postgreSQL_group_Query,connection)
+    df_student = pd.read_sql_query(postgreSQL_group_Query,connection)
     df_project = pd.read_sql_query(postgreSQL_project_Query,connection)
+    df_student['details'] = df_student['course_taken'] + " " + df_student['specialization'] + " " + df_student['area_of_interest'] 
+    df_project['details'] = df_project['title'] + " " + df_project['overview'] + " " + df_project['requirements'] 
+    df_group = pd.DataFrame(columns=['id','details'])
+    df_group['id'] = group_id
+    for row in df_student.iterrows():
+        df_group['details'] = row['details']
 
-
+    
